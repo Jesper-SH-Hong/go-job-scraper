052ea0dbeebdd448922a252ef7a27e8fbaff58d6
Jesper H <seonghyun91@gmail.com>
2024-01-09 06:13:19 -0800

    fix comment

--- main.go
+++ main.go
@@ -8,10 +8,11 @@ import (
 // import "main/synchronous"
 
 // step2
-// 주고받을 자료 bool
-func isGood(person string, c chan bool) {
+// 주고받을 자료 string
+func isGood(person string, c chan string) {
 	time.Sleep(time.Second * 2)
-	c <- true //send true to channel a.k.a. c
+	// c <- true //send true to channel a.k.a. c
+	c <- person + " is great!"
 }
 
 // step1
@@ -20,15 +21,17 @@ func main() {
 
 	//gorountine은 프로그램이 작동할동안만 live.(즉 메인함수와 함께 꺼짐)
 	//기다려주지 않음.
-	//Channel은 고루틴간, 고루틴-메인 간 커뮤니케이션 가능
 
-	//NOTE: 어떤 종류의 자료를 주고받을 것인가 bool
-	c := make(chan bool)
+	//Channel은 고루틴간, 고루틴-메인 간 커뮤니케이션 가능
+	//NOTE: 어떤 종류의 자료를 주고받을 것인가 string
+	c := make(chan string)
 
 	people := [2]string{"jesper", "flynn"}
 
-	//2개의 고루틴 생성
+	//TODO: 2개의 고루틴 생성
 	for _, person := range people {
+
+		//NOTE: 고루틴은 go 키워드
 		//고루틴이니 result := go isGood(person) 이런 거 불가.. 받기 전에 끝날 수도
 		//고로 저 채널을 isGood으로 보내서 메인 함수와 연계.
 		go isGood(person, c)
@@ -40,7 +43,7 @@ func main() {
 
 	fmt.Println(result)
 	fmt.Println(<-c) //남은 한 고루틴 메시지 받기. 둘 다 거의 동시에 끝남.
-	// fmt.Println(<-c) //all gourintes are asleep - deadlock ERROR. 고루틴 2개 돌렸음..
+	// fmt.Println(<-c) //all gourintes are asleep - deadlock ERROR. 고루틴 2개 돌렸음.. 받을 게 없는데 대기.. 안 끝남. 고로 에러 내줌
 	
 
 }
